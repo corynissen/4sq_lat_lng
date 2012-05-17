548b24edeef5944dab4967a855bab31c3433b6e2
cory nissen <corynissen@gmail.com>
2012-05-17 14:46:51 -0500

    updated go.R with the cluster function

--- go.R
+++ go.R
@@ -10,6 +10,9 @@ username <- "michaelmcgee"
 # takes time to run... lots of api calls to resolve t.co links
 latlngs <- getLatLngs(username, n=100)
 
+# cluster those latlngs and get a center
+clusterCenter <- getClusterCenter(latlngs)
+
 # see the latlngs on a map...
 # make sure latlngs isn't empty for the username you've chosen
-makeGraph(latlngs, zoom=3)
+makeGraph(latlngs, zoom=3) + addPoint(clusterCenter, size=3, colour="blue")
